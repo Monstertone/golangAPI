79e6c256b41b0097532489d5857ab70c3ca16a45
Thomas Gabauer <tgabauer@gmail.com>
2018-12-10 19:47:42 -0700

    creating folder structure

--- main.go
+++ main.go
@@ -1,38 +1,55 @@
 package main
 
 import (
-  "encoding/json"
+
   "log"
+  "os"
+  "database/sql"
+  "encoding/json"
   "net/http"
   "github.com/gorilla/mux"
-  "strconv"
+  "github.com/lib/pq"
+  "github.com/subosito/gotenv"
 
-  _ "database/sql"
-  _ "github.com/lib/pq"
-  _ "github.com/subosito/gotenv"
+
+
 )
 
 type Transaction struct {
   ID             int     `json:id`
-  Users_ID       int     `json:users_ID`
-  Amount         string  `json:amount`
-  Type           string  `json:type`
+  Amount         int     `json:amount`
   Business_name  string  `json:business_name`
+  Type_trans     string  `json:type`
+  Users_ID       int     `json:users_id`
 }
 
 var transactions []Transaction
+var db *sql.DB
+
+func init(){
+  gotenv.Load()
+}
+
+func logFatal(err error){
+  if err != nil {
+    log.Fatal(err)
+  }
+}
 
 func main() {
+
+  pgUrl, err := pq.ParseURL(os.Getenv("POSTGRESQL_URL"))
+  logFatal(err)
+
+  db, err = sql.Open("postgres", pgUrl)
+  logFatal(err)
+
+  err = db.Ping()
+  logFatal(err)
+
+  log.Println(pgUrl)
+
   router := mux.NewRouter()
-
-  transactions = append(transactions,
-Transaction{ID: 1, Users_ID: 1, Amount: "44.97", Type:"gasoline", Business_name:"Costco"},
-Transaction{ID: 2, Users_ID: 2, Amount: "16.99", Type:"paper supplies", Business_name:"Office Depot"},
-Transaction{ID: 3, Users_ID: 3, Amount: "108.43", Type:"computer parts", Business_name:"Frys Electronics"},
-Transaction{ID: 4, Users_ID: 1, Amount: "79.99", Type:"printer", Business_name:"Frys Electronics"},
-Transaction{ID: 5, Users_ID: 3, Amount: "34.27", Type:"learning courses", Business_name:"Udemy"},
-Transaction{ID: 6, Users_ID: 3, Amount: "299.99", Type:"galaga machine", Business_name:"Walmart"})
-
 
   router.HandleFunc("/transactions", getTransactions).Methods("GET")
   router.HandleFunc("/transactions/{id}", getTransaction).Methods("GET")
@@ -44,57 +61,78 @@ Transaction{ID: 6, Users_ID: 3, Amount: "299.99", Type:"galaga machine", Busines
 }
 
 func getTransactions(w http.ResponseWriter, r *http.Request) {
-  json.NewEncoder(w).Encode(transactions)
+  var transaction Transaction
+  transactions = []Transaction{}
+
+  rows, err := db.Query("select * from transactions")
+  logFatal(err)
+
+  defer rows.Close()
+
+  for rows.Next(){
+    err :=rows.Scan(&transaction.ID, &transaction.Amount, &transaction.Business_name,&transaction.Type_trans, &transaction.Users_ID)
+    logFatal(err)
+
+    transactions = append(transactions, transaction)
+  }
+   json.NewEncoder(w).Encode(transactions)
 }
+
 func getTransaction(w http.ResponseWriter, r *http.Request) {
+  var transaction Transaction
   params := mux.Vars(r)
 
-  i, _ := strconv.Atoi(params["id"])
+  rows := db.QueryRow("select * from transactions where id=$1", params["id"])
 
-  for _, transaction := range transactions {
-    if transaction.ID == i {
-      json.NewEncoder(w).Encode(&transaction)
-    }
-  }
+  err := rows.Scan(&transaction.ID, &transaction.Amount, &transaction.Business_name, &transaction.Type_trans, &transaction.Users_ID)
+  logFatal(err)
+
+  json.NewEncoder(w).Encode(transaction)
+
 
 }
 
 func addTransaction(w http.ResponseWriter, r *http.Request) {
   var transaction Transaction
+  var transactionID int
+
   json.NewDecoder(r.Body).Decode(&transaction)
 
-  transactions = append(transactions, transaction)
+  err := db.QueryRow("insert into transactions (id, amount, business_name, type_trans, users_id) values ($1, $2, $3, $4, $5) RETURNING id;",
+              transaction.ID, transaction.Amount, transaction.Business_name, transaction.Type_trans, transaction.Users_ID).Scan(&transactionID)
 
-  json.NewEncoder(w).Encode(transactions)
-  log.Println(transaction)
+  logFatal(err)
+
+  json.NewEncoder(w).Encode(transactionID)
+
 }
 
 func updateTransaction(w http.ResponseWriter, r *http.Request) {
   var transaction Transaction
-
   json.NewDecoder(r.Body).Decode(&transaction)
 
-        for i, item := range transactions {
-        if item.ID == transaction.ID {
-          transactions[i] = transaction
+  result, err := db.Exec("update transactions set amount=$1, business_name=$2, type_trans=$3, users_id=$4 where id=$5 RETURNING id",
+  &transaction.Amount, &transaction.Business_name, &transaction.Type_trans, &transaction.Users_ID, &transaction.ID)
 
-        }
-    }
+rowsUpdated, err := result.RowsAffected()
+logFatal(err)
 
-    json.NewEncoder(w).Encode(transactions)
+json.NewEncoder(w).Encode(rowsUpdated)
+
 }
+
+
 
 func deleteTransaction(w http.ResponseWriter, r *http.Request) {
   params := mux.Vars(r)
+  log.Println(params["id"])
 
-  id, _ := strconv.Atoi(params["id"])
+  result, err := db.Exec("delete from transactions where id=$1", params["id"])
+  logFatal(err)
 
-  for i, item := range transactions {
-    if item.ID == id {
-      transactions = append(transactions[:i], transactions[i+1:]...)
-    }
-  }
+  rowsDeleted, err := result.RowsAffected()
+  logFatal(err)
 
-  json.NewEncoder(w).Encode(transactions)
+  json.NewEncoder(w).Encode(rowsDeleted)
 
 }
